4e682ddad636bd56e2295551fde33e80e725ac25
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-03-03 00:52:28 +0000

    added html layout for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import Image from "next/image";
+
+export default function Page() {
   return (
-    <main>
-      <h1>Test</h1>
-    </main>
+      <main>
+          <h1>VOX MATCH</h1>
+          <button>LOG IN</button>
+          <Image
+              src="/zto-logo.png"
+              width={300}
+              height={300}
+              alt="Company logo"
+          />
+      </main>
   );
 }
