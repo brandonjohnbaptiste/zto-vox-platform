fcfee91dda4f264573aa16288989f533649f9299
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-03-03 19:29:21 +0000

    v1 of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <main className="grid grid-rows-10 h-[90%] w-[90%] grid-cols-4 place-items-center bg-background-light rounded-md self-center mx-auto drop-shadow-2xl">
           <div className="z-20 row-span-7 col-span-4 w-full h-full flex flex-col items-center justify-center">
               <h1 className="mb-10 drop-shadow-xl text-center text-8xl cols-span-4 text-transparent font-bold bg-clip-text bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">VOX MATCH</h1>
-              <Button className="text-white cols-span-2  p-4 bg-accent  hover:scale-110">SIGN IN</Button>
+              <Button className="text-white cols-span-2 text-[1rem]  p-4 bg-accent  hover:scale-110">SIGN IN</Button>
           </div>
           <Image
               className="row-span-2 col-span-4"
