c1df27e2e3b8ee02880311b8b925fa6edab3d9c6
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-03-03 01:17:07 +0000

    added styling for logo and header text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-      <main>
-          <h1>VOX MATCH</h1>
-          <button>LOG IN</button>
+      <main className="grid grid-rows-10 grid-cols-4 h-screen place-items-center items-center">
+          <div className="row-span-7 col-span-4">
+              <h1 className="text-center text-8xl cols-span-4 font-bold">VOX MATCH</h1>
+              <button className="cols-span-2">LOG IN</button>
+          </div>
           <Image
+              className="row-span-2 col-span-4"
               src="/zto-logo.png"
               width={300}
               height={300}
