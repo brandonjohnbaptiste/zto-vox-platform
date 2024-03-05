78dc81da58f2bdda8422d704eae374c17801b635
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-03-05 17:15:01 +0000

    added vocal match logo

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import Image from "next/image";
+import {Bebas_Neue} from "next/font/google";
 
+const bebasNeue = Bebas_Neue({ weight: "400", subsets: ["latin"]});
 
 export const metadata: Metadata = {
     title: "Vox Match",
@@ -25,12 +26,7 @@ export default function HomeLayout({
                         height={200}
                         alt='company logo'
                     />
-                    <Image
-                        src='/zto-logo.png'
-                        width={200}
-                        height={200}
-                        alt='company logo'
-                    />
+                    <h2 className={`${bebasNeue.className} text-white text-[8rem] mx-auto self-center`} >VM</h2>
 
                 </div>
                 <div className="w-[400px] h-[750px] bg-background-light m-5 rounded-md"></div>
