d7df333b57c93ef44edfc907cd71b3a3f2d447d4
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-03-08 01:27:06 +0000

    setting up routes for cookie sessions

---
+++ src/auth/callback/route.ts
@@ -0,0 +1,18 @@
+import {cookies} from "next/headers";
+import {createRouteHandlerClient} from "@supabase/auth-helpers-nextjs";
+import {NextResponse} from "next/server";
+
+export async function GET(request) {
+    const requestUrl = new URL(request.url);
+    const code = requestUrl.searchParams.get('code');
+
+    // setup supabase to use cookies
+    if (code) {
+        const cookieStore = cookies();
+        const supabase = createRouteHandlerClient({cookies: () => cookieStore});
+
+        await supabase.auth.exchangeCodeForSession(code);
+    }
+
+    return NextResponse.redirect(requestUrl.origin);
+}
