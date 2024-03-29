84d8cf3a3a144e1612dd5806045e6e2d30ce08b4
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-03-29 02:03:54 +0000

    fixed essentia imports and adding audio context api

--- src/scripts/audioEngine.ts
+++ src/scripts/audioEngine.ts
@@ -1,13 +1,10 @@
 'use server'
-let esPkg = require('essentia.js');
-
-
-
-
+import * as es from 'essentia.js';
+import * as tf from '@tensorflow/tfjs';
 
 
 export async function runAnalysis() {
-    const essentia = await new esPkg.Essentia(esPkg.EssentiaWASM);
+    const essentia = await new es.Essentia(es.EssentiaWASM);
     console.log(essentia.version)
 
     console.log(essentia.algorithmNames)
