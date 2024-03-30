3a573f1d762a8dca1f86ce6dfbe8ee9b34de810f
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-03-30 22:12:24 +0000

    calling bpm extraction and displaying results on client-side

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,24 +1,28 @@
 'use client'
 import {Button} from "@/components/ui/moving-border";
-import {RunAnalysis} from "@/scripts/audioEngine";
 import loadModel from "@/scripts/audioUtils";
+import {useState} from "react";
+import {RunAnalysis} from "@/scripts/audioEngine";
 
 
 export default function Page() {
+    const [bpm, setBpm] = useState('0');
+
+    const calcBpm = async () => {
+        let modelBpm = await RunAnalysis();
+        console.log(modelBpm);
+        setBpm(`${modelBpm.toFixed(0)}`);
+    }
+
+
+
+
     return (
         <>
             <h1>DASHBOARD</h1>
-            <Button onClick={RunTest}>RUN</Button>
+            <Button onClick={calcBpm}>RUN</Button>
+            <h2>{bpm}</h2>
         </>
 
     )
-}
-
-
-function RunTest() {
-    const audioCtx = new AudioContext();
-    console.log(audioCtx)
-
-
-    loadModel();
 }
--- src/scripts/audioEngine.ts
+++ src/scripts/audioEngine.ts
@@ -9,14 +9,19 @@ import {promises as fs} from 'fs';
 
 export async function RunAnalysis() {
     const essentia = await new es.Essentia(es.EssentiaWASM);
-    console.log(essentia.version)
+    //console.log(essentia.version)
 
-    const buffer = await fs.readFile(process.cwd() + '/src/audioStore/replay-95bpm.wav');
-    const audio = wav.decode(buffer);
-    console.log(buffer);
-    console.log(audio);
+    let buffer = await fs.readFile(process.cwd() + '/src/audioStore/replay-95bpm.wav');
+    let audio = wav.decode(buffer);
+    //console.log(buffer);
+    //console.log(audio);
+
+    const audioVector = essentia.arrayToVector(audio.channelData[0]);
+    //console.log(audioVector);
 
 
+    let bpm = await essentia.RhythmExtractor(audioVector);
+    console.log(bpm.bpm);
 
-
+    return bpm.bpm;
 }
