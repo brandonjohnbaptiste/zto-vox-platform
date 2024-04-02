0738e5b66e8debcfef9e73e293286586d528f2bf
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-04-02 17:04:38 +0100

    adding gradient border to engine result card

--- src/components/ui/engine-output.tsx
+++ src/components/ui/engine-output.tsx
@@ -1,9 +1,15 @@
 export default function EngineOutput() {
     return (
         <>
-            <div className="bg-grey w-[20%] h-[40vh] rounded-md m-5">
+            <div className="bg-gradient-to-b from-accent via-pink-accent/80 to-accent/30 w-[20%] h-[40vh] p-1 rounded-md drop-shadow-xl">
+                <div className="bg-grey w-[100%] h-[100%] rounded-md">
 
+               </div>
             </div>
+
+
+
+
         </>
     )
 }
