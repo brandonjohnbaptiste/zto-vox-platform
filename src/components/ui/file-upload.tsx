75243fcdb9261744067edc9668d899c1e1cdfe0e
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-04-05 03:29:30 +0100

    adding support for file uploads via uppy

--- src/components/ui/file-upload.tsx
+++ src/components/ui/file-upload.tsx
@@ -1,7 +1,11 @@
+import Uploader from "@/components/ui/uploader";
+
 export default function FileUpload() {
     return (
         <>
-            <div className="w-[100%] h-[20vh] bg-grey rounded-xl drop-shadow-xl mt-5"></div>
+            <div className="w-[100%] bg-grey rounded-xl drop-shadow-xl mt-5">
+                <Uploader className="w-[50%] h-[50%]"/>
+            </div>
         </>
     )
 }
---
+++ src/components/ui/uploader.tsx
@@ -0,0 +1,13 @@
+'use client';
+import {useState} from "react";
+import Uppy from "@uppy/core";
+import {Dashboard} from "@uppy/react";
+
+import '@uppy/core/dist/style.min.css';
+import '@uppy/dashboard/dist/style.min.css';
+
+export default function Uploader() {
+    const [uppy] = useState(() => new Uppy());
+
+    return <Dashboard uppy={uppy} />
+}
