fcf5e27bebec6881bbdbd6b2d246be2f9384b9e0
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-04-05 05:04:44 +0100

    overriding uppy default styles

--- src/components/ui/file-upload.tsx
+++ src/components/ui/file-upload.tsx
@@ -3,8 +3,8 @@ import Uploader from "@/components/ui/uploader";
 export default function FileUpload() {
     return (
         <>
-            <div className="w-[100%] bg-grey rounded-xl drop-shadow-xl mt-5">
-                <Uploader className="w-[50%] h-[50%]"/>
+            <div className="w-[100%] bg-grey rounded-xl drop-shadow-xl mt-5 p-5">
+                <Uploader/>
             </div>
         </>
     )
--- src/components/ui/uploader.tsx
+++ src/components/ui/uploader.tsx
@@ -1,13 +1,12 @@
 'use client';
 import {useEffect, useState} from "react";
-import Uppy from "@uppy/core";
+import Uppy, {UppyOptions} from "@uppy/core";
 import {Dashboard} from "@uppy/react";
 
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 
 export default function Uploader() {
-    const [uppy] = useState(() => new Uppy());
-
-    return <Dashboard uppy={uppy} theme="dark"/>
+    const [uppy] = useState(() => new Uppy({restrictions: {maxNumberOfFiles: 1}}));
+    return <Dashboard uppy={uppy} theme="dark" height={300} width={300}/>
 }
