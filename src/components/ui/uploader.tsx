355422066b049de51749527dff83a81f312f5733
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-04-06 04:19:38 +0100

    added upload function logic for upload component

--- src/components/ui/uploader.tsx
+++ src/components/ui/uploader.tsx
@@ -1,12 +1,45 @@
 'use client';
 import {useEffect, useState} from "react";
 import Uppy, {UppyOptions} from "@uppy/core";
+import Tus from "@uppy/tus";
 import {Dashboard} from "@uppy/react";
 
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 
 export default function Uploader() {
-    const [uppy] = useState(() => new Uppy({restrictions: {maxNumberOfFiles: 1}}));
+    let [uppy] = useState(() => new Uppy(
+        {restrictions: {maxNumberOfFiles: 1}}
+        ).use(Tus, {
+            endpoint: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/upload/resumable`,
+            headers: {
+                authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`
+            },
+            chunkSize: 6 * 1024 * 1024,
+            allowedMetaFields: ['bucketName', 'objectName', 'contentType', 'cacheControl'],
+        })
+    );
+
+    uppy.on('file-added', (file) => {
+        const supabaseMetadata = {
+            bucketName: 'sample',
+            objectName: file.name,
+            contentType: file.type
+        }
+
+        file.meta = {
+            ...file.meta,
+            ...supabaseMetadata
+        }
+
+        console.log('file added', file)
+    });
+
+    uppy.on('complete', (result) => {
+        console.log('Upload complete, uploaded files:', result.successful);
+    });
+
+
+
     return <Dashboard uppy={uppy} theme="dark" height={300} width={300}/>
 }
