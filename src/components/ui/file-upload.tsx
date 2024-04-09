40b3960f859c1bc6fb4baf70bafcfa08aa236ca9
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2024-04-09 04:02:05 +0100

    displaying all samples user has

--- src/components/ui/file-upload.tsx
+++ src/components/ui/file-upload.tsx
@@ -1,23 +1,23 @@
 'use client'
 import Uploader from "@/components/ui/uploader";
 import {createClient} from "@/utils/supabase/client";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function FileUpload() {
 
     const supabase = createClient();
+    const [userSamples, setUserSamples] = useState([]);
 
     async function grabUserData() {
 
         const {data: {user}} = await supabase.auth.getUser();
-        console.log(user.id);
 
         const {data, err} = await supabase
             .from('samples')
             .select()
             .eq('created_by', user.id);
 
-        console.log(data);
+        setUserSamples(data);
     }
 
 
@@ -29,6 +29,9 @@ export default function FileUpload() {
         <>
             <div className="w-[100%] bg-grey rounded-xl drop-shadow-xl mt-5 p-5">
                 <Uploader/>
+                {userSamples.map(sample => (
+                    <p key={sample.id}>{sample.file_name}</p>
+                ))}
             </div>
         </>
     )
